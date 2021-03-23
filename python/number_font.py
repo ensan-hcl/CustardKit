336df81c25adf538d241ae2ddb03e7890c7f985e
En3_HCl <En3_HCl_K@icloud.com>
2021-03-23 12:40:32 +0900

    format

--- python/number_font.py
+++ python/number_font.py
@@ -1,17 +1,19 @@
 from source.custard import *
 
-def pc_style_input_key(char: str) -> CustomKey :
+
+def pc_style_input_key(char: str) -> CustomKey:
     return CustomKey(
-        design = KeyDesign(
-            label = TextLabel(text = char),
-            color = KeyColor.normal
+        design=KeyDesign(
+            label=TextLabel(text=char),
+            color=KeyColor.normal
         ),
-        press_actions = [
+        press_actions=[
             action_input(char)
         ],
-        longpress_actions = LongpressAction(),
-        variations = []
+        longpress_actions=LongpressAction(),
+        variations=[]
     )
+
 
 keys: list[KeyData] = []
 
@@ -26,87 +28,87 @@ for i in range(len(numbers)):
     for j in range(len(numbers[i])):
         x, y = (j, i)
         keyData = KeyData(
-            specifier = GridFitSpecifier(x = x, y = y),
-            key = pc_style_input_key(numbers[i][j])
+            specifier=GridFitSpecifier(x=x, y=y),
+            key=pc_style_input_key(numbers[i][j])
         )
         keys.append(keyData)
 
 keys.append(KeyData(
-    specifier = GridFitSpecifier(x = 0, y = 4),
-    key = CustomKey(
-        design = KeyDesign(
-            label = SystemImageLabel("list.bullet"),
-            color = KeyColor.special
+    specifier=GridFitSpecifier(x=0, y=4),
+    key=CustomKey(
+        design=KeyDesign(
+            label=SystemImageLabel("list.bullet"),
+            color=KeyColor.special
         ),
-        press_actions = [
+        press_actions=[
             action_toggle_tab_bar()
         ],
-        longpress_actions = LongpressAction(),
-        variations = []
+        longpress_actions=LongpressAction(),
+        variations=[]
     )
 ))
 
 keys.append(KeyData(
-    specifier = GridFitSpecifier(x = 1, y = 4),
-    key = SystemKey(SystemKeyType.change_keyboard)
+    specifier=GridFitSpecifier(x=1, y=4),
+    key=SystemKey(SystemKeyType.change_keyboard)
 ))
 
 keys.append(KeyData(
-    specifier = GridFitSpecifier(x = 2, y = 4),
-    key = CustomKey(
-        design = KeyDesign(
-            label = SystemImageLabel("delete.left"),
-            color = KeyColor.special
+    specifier=GridFitSpecifier(x=2, y=4),
+    key=CustomKey(
+        design=KeyDesign(
+            label=SystemImageLabel("delete.left"),
+            color=KeyColor.special
         ),
-        press_actions = [
+        press_actions=[
             action_delete(1)
         ],
-        longpress_actions = LongpressAction(
-            repeat = [
+        longpress_actions=LongpressAction(
+            repeat=[
                 action_delete(1)
             ]
         ),
-        variations = []
+        variations=[]
     )
 ))
 
 keys.append(KeyData(
-    specifier = GridFitSpecifier(x = 3, y = 4, width = 4, height = 1),
-    key = CustomKey(
-        design = KeyDesign(
-            label = TextLabel("空白"),
-            color = KeyColor.normal
+    specifier=GridFitSpecifier(x=3, y=4, width=4, height=1),
+    key=CustomKey(
+        design=KeyDesign(
+            label=TextLabel("空白"),
+            color=KeyColor.normal
         ),
-        press_actions = [
+        press_actions=[
             action_input(" ")
         ],
-        longpress_actions = LongpressAction(
-            start = [
+        longpress_actions=LongpressAction(
+            start=[
                 action_toggle_cursor_bar()
             ]
         ),
-        variations = []
+        variations=[]
     )
 ))
 
 keys.append(KeyData(
-    specifier = GridFitSpecifier(x = 7, y = 4, width = 3, height = 1),
-    key = SystemKey(SystemKeyType.enter)
+    specifier=GridFitSpecifier(x=7, y=4, width=3, height=1),
+    key=SystemKey(SystemKeyType.enter)
 ))
 
 #カスタードオブジェクトを作成
 hieroglyphs_custard = Custard(
-    identifier = "number_font",
-    language = Language.none,
-    input_style = InputStyle.direct,
-    metadata = Metadata(
-        custard_version = "1.0",
-        display_name = "装飾数字",
+    identifier="number_font",
+    language=Language.none,
+    input_style=InputStyle.direct,
+    metadata=Metadata(
+        custard_version="1.0",
+        display_name="装飾数字",
     ),
-    interface = Interface(
-        key_style = KeyStyle.pc_style,
-        key_layout = GridFitLayout(row_count = 10, column_count = 5),
-        keys = keys
+    interface=Interface(
+        key_style=KeyStyle.pc_style,
+        key_layout=GridFitLayout(row_count=10, column_count=5),
+        keys=keys
     )
 )
-hieroglyphs_custard.write(to = "./results/number_font.json")
+hieroglyphs_custard.write(to="./results/number_font.json")
