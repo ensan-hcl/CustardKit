4efb3d59b3a4ef45bab9602f3805f5ce6c5e0ddf
En3_HCl <En3_HCl_K@icloud.com>
2021-03-23 17:01:19 +0900

    refine mdboard example

--- swift/examples/MdKeyboard.swift
+++ swift/examples/MdKeyboard.swift
@@ -1,86 +1,60 @@
 import Foundation
 import CustardKit
 
-func makeKey(main: String, sub: [String]?) -> CustardInterfaceKey {
-    if let sub = sub{
-        return .custom(
-            .init(
-                design: .init(label: .text(main), color: .normal),
-                press_actions: [.input(main)],
-                longpress_actions: .none,
-                variations: [
-                    .init(type: .flickVariation(.left), key: .init(design: .init(label: .text(sub[0])), press_actions: [.input(sub[0])], longpress_actions: .none)),
-                    .init(type: .flickVariation(.top), key: .init(design: .init(label: .text(sub[1])), press_actions: [.input(sub[1])], longpress_actions: .none)),
-                    .init(type: .flickVariation(.right), key: .init(design: .init(label: .text(sub[2])), press_actions: [.input(sub[2])], longpress_actions: .none)),
-                    .init(type: .flickVariation(.bottom), key: .init(design: .init(label: .text(sub[3])), press_actions: [.input(sub[3])], longpress_actions: .none))
-                ])
-        )}
-    else{
-        return .custom(
-            .init(
-                design: .init(label: .text(main), color: .normal),
-                press_actions: [.input(main)],
-                longpress_actions: .none,
-                variations: [])
+func makeFlickInputKey(center: String, subs: [String]) -> CustardInterfaceKey {
+    let variations: [CustardInterfaceVariation] = zip(subs, [FlickDirection.left, .top, .right, .bottom]).map{letter, direction in
+        .init(
+            type: .flickVariation(direction),
+            key: .init(
+                design: .init(label: .text(letter)),
+                press_actions: [.input(letter)],
+                longpress_actions: .none
+            )
         )
     }
+
+    return .custom(
+        .init(
+            design: .init(label: .text(center), color: .normal),
+            press_actions: [.input(center)],
+            longpress_actions: .none,
+            variations: variations
+        )
+    )
 }
 
 let md_custard = Custard(
-    identifier: "md_keyboard_jp",
+    identifier: "flick_md_ja_JP",
     language: .ja_JP,
     input_style: .direct,
-    metadata: .init(custard_version: .v1_0, display_name: "日本語markdownキーボード"),
+    metadata: .init(custard_version: .v1_0, display_name: "markdown"),
     interface: .init(
         keyStyle: .tenkeyStyle,
         keyLayout: .gridFit(.init(rowCount: 6, columnCount: 4)),
         keys: [
-            .gridFit(.init(x: 0, y: 0)): .custom(
-                .init(
-                    design: .init(label: .text("123"), color: .special),
-                    press_actions: [.moveTab(.system(.flick_numbersymbols))],
-                    longpress_actions: .init(start: [.toggleTabBar]),
-                    variations: []
-                )
-            ),
-            .gridFit(.init(x: 0, y: 1)): .custom(
-                .init(
-                    design: .init(label: .text("abc"), color: .special),
-                    press_actions: [.moveTab(.system(.user_english))],
-                    longpress_actions: .none,
-                    variations: [
-                        .init(
-                            type: .flickVariation(.right),
-                            key: .init(
-                                design: .init(label: .text("→")),
-                                press_actions: [.moveCursor(1)],
-                                longpress_actions: .init(repeat: [.moveCursor(1)])
-                            )
-                        )
-                    ]
-                )
-            ),
+            .gridFit(.init(x: 0, y: 0)): .system(.flick_star123_tab),
+            .gridFit(.init(x: 0, y: 1)): .system(.flick_abc_tab),
             .gridFit(.init(x: 0, y: 2)): .custom(
                 .init(
-                    design: .init(label: .text("あいう"), color: .special),
-                    press_actions: [.moveTab(.system(.user_japanese))],
+                    design: .init(label: .text("あいう"), color: .selected),
+                    press_actions: [.moveTab(.custom("md_keyboard_jp"))],
                     longpress_actions: .none,
                     variations: []
                 )
             ),
             .gridFit(.init(x: 0, y: 3)): .system(.change_keyboard),
-            .gridFit(.init(x: 1, y: 0)): makeKey(main: "あ", sub: ["い", "う", "え", "お"]),
-            .gridFit(.init(x: 2, y: 0)): makeKey(main: "か", sub: ["き", "く", "け", "こ"]),
-            .gridFit(.init(x: 3, y: 0)): makeKey(main: "さ", sub: ["し", "す", "せ", "そ"]),
-            .gridFit(.init(x: 1, y: 1)): makeKey(main: "た", sub: ["ち", "つ", "て", "と"]),
-            .gridFit(.init(x: 2, y: 1)): makeKey(main: "な", sub: ["に", "ぬ", "ね", "の"]),
-            .gridFit(.init(x: 3, y: 1)): makeKey(main: "は", sub: ["ひ", "ふ", "へ", "ほ"]),
-            .gridFit(.init(x: 1, y: 2)): makeKey(main: "ま", sub: ["み", "む", "め", "も"]),
-            .gridFit(.init(x: 2, y: 2)): makeKey(main: "や", sub: ["「", "ゆ", "」", "よ"]),
-            .gridFit(.init(x: 3, y: 2)): makeKey(main: "ら", sub: ["り", "る", "れ", "ろ"]),
+            .gridFit(.init(x: 1, y: 0)): makeFlickInputKey(center: "あ", subs: ["い", "う", "え", "お"]),
+            .gridFit(.init(x: 2, y: 0)): makeFlickInputKey(center: "か", subs: ["き", "く", "け", "こ"]),
+            .gridFit(.init(x: 3, y: 0)): makeFlickInputKey(center: "さ", subs: ["し", "す", "せ", "そ"]),
+            .gridFit(.init(x: 1, y: 1)): makeFlickInputKey(center: "た", subs: ["ち", "つ", "て", "と"]),
+            .gridFit(.init(x: 2, y: 1)): makeFlickInputKey(center: "な", subs: ["に", "ぬ", "ね", "の"]),
+            .gridFit(.init(x: 3, y: 1)): makeFlickInputKey(center: "は", subs: ["ひ", "ふ", "へ", "ほ"]),
+            .gridFit(.init(x: 1, y: 2)): makeFlickInputKey(center: "ま", subs: ["み", "む", "め", "も"]),
+            .gridFit(.init(x: 2, y: 2)): makeFlickInputKey(center: "や", subs: ["「", "ゆ", "」", "よ"]),
+            .gridFit(.init(x: 3, y: 2)): makeFlickInputKey(center: "ら", subs: ["り", "る", "れ", "ろ"]),
             .gridFit(.init(x: 1, y: 3)): .system(.flick_kogaki),
-            .gridFit(.init(x: 2, y: 3)): makeKey(main: "わ", sub: ["を", "ん", "ー", ""]),
-            .gridFit(.init(x: 3, y: 3)): makeKey(main: "、", sub: ["。", "！", "？", ""]),
+            .gridFit(.init(x: 2, y: 3)): makeFlickInputKey(center: "わ", subs: ["を", "ん", "ー", ""]),
+            .gridFit(.init(x: 3, y: 3)): .system(.flick_kutoten),
             .gridFit(.init(x: 4, y: 0)): .custom(
                 .init(
                     design: .init(label: .systemImage("bold"), color: .special),
@@ -147,6 +121,14 @@ let md_custard = Custard(
                         .init(
                             type: .flickVariation(.right),
                             key: .init(
+                                design: .init(label: .text(">")),
+                                press_actions: [.input(">")],
+                                longpress_actions: .none
+                            )
+                        ),
+                        .init(
+                            type: .flickVariation(.bottom),
+                            key: .init(
                                 design: .init(label: .text("-")),
                                 press_actions: [.input("-")],
                                 longpress_actions: .none
@@ -200,9 +182,24 @@ let md_custard = Custard(
             .gridFit(.init(x: 4, y: 3)): .custom(
                 .init(
                     design: .init(label: .text("```"), color: .special),
-                    press_actions: [.input("```\n\n```"), .moveCursor(-5)],
+                    press_actions: [.input("```\n\n```"), .complete, .moveCursor(-4)],
                     longpress_actions: .none,
-                    variations: []
+                    variations: [
+                        .init(
+                            type: .flickVariation(.top),
+                            key: .init(
+                                design: .init(label: .systemImage("tablecells")),
+                                press_actions: [.input("""
+                                        |a  |b  |c  |
+                                        |---|---|---|
+                                        |   |   |   |
+                                        |   |   |   |
+                                    """), .complete],
+                                longpress_actions: .none
+                            )
+                        )
+
+                    ]
                 )
             ),
             .gridFit(.init(x: 5, y: 0)): .custom(
@@ -219,14 +216,6 @@ let md_custard = Custard(
                                 longpress_actions: .none
                             )
                         ),
-                        .init(
-                            type: .flickVariation(.bottom),
-                            key: .init(
-                                design: .init(label: .systemImage("delete.right")),
-                                press_actions: [.delete(-1)],
-                                longpress_actions: .init(repeat: [.moveCursor(-1)])
-                            )
-                        )
                     ]
                 )
             ),
@@ -247,26 +236,27 @@ let md_custard = Custard(
                         .init(
                             type: .flickVariation(.top),
                             key: .init(
-                                design: .init(label: .text("\t")),
-                                press_actions: [.moveCursor(-1)],
+                                design: .init(label: .text("全角")),
+                                press_actions: [.input("　")],
+                                longpress_actions: .none
+                            )
+                        ),
+                        .init(
+                            type: .flickVariation(.bottom),
+                            key: .init(
+                                design: .init(label: .text("tab")),
+                                press_actions: [.input("\t")],
                                 longpress_actions: .none
                             )
                         )
                     ]
                 )
             ),
-            .gridFit(.init(x: 5, y: 2)): .custom(
-                .init(
-                    design: .init(label: .systemImage("list.bullet"), color: .special),
-                    press_actions: [.toggleTabBar],
-                    longpress_actions: .none,
-                    variations: []
-                )
-            ),
-            .gridFit(.init(x: 5, y: 3, width: 1, height: 1)): .system(.enter),
+            .gridFit(.init(x: 5, y: 2, width: 1, height: 2)): .system(.enter),
         ]
     )
 )
+
 do {
     try md_custard.write(to: URL(fileURLWithPath: "../results/md_keyboard.json"))
 } catch {
